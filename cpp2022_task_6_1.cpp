8a13ac6dedd29a87c26e1b1a567651d186917215
FluFFka <iliasfedorenko@mail.ru>
2022-11-25 20:14:30 +0300

    Completed 1 task

---
+++ cpp2022_task_6_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <type_traits>
+#include <tuple>
+
+template <char ...c> struct TString { };
+
+template <typename T, T ...c>
+constexpr TString<c...> operator"" _s() {
+    return TString<c...>();
+}
+
+template <char ...lc, char ...rc>
+constexpr TString<lc..., rc...> operator+(const TString<lc...> &lhs, const TString<rc...> &rhs) {
+    return TString<lc..., rc...>();
+}
+
+
+template <char ...lc, char ...rc>
+constexpr bool operator==(const TString<lc...> &lhs, const TString<rc...> &rhs) {
+    return std::is_same_v<TString<lc...>, TString<rc...>>;
+}
+
+
+int main()
+{
+    constexpr auto hello = "hello"_s + " world"_s;
+    static_assert(hello == "hello world"_s);
+    return 0;
+}
